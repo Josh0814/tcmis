a8a67198c0ea6201dda6e514bdac463d0dae278b
Josh0814 <a43236080@gmail.com>
2026-05-14 12:11:09 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -58,7 +58,7 @@ def webhook():
         rate =  req["queryResult"]["parameters"]["rate"]
         info = "我是黃建鴻設計的機器人,您選擇的電影分級是：" + rate  + "，相關電影：\n"
         db = firestore.client()
-        collection_ref = db.collection("本周新片含分級")
+        collection_ref = db.collection("本週新片含分級")
         docs = collection_ref.get()
         result = ""
         for doc in docs:
